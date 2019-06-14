3e6303e3973892b623e8a8ec016e1bc8cee6deb4
漆家佳 <88423274@cnsuning.com>
2019-06-14 19:37:04 +0800

    上传swift版基类框架

---
+++ JASwiftBase.framework/Headers/JASwiftBase.h
@@ -0,0 +1,14 @@
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JASwiftBase.
+FOUNDATION_EXPORT double JASwiftBaseVersionNumber;
+
+//! Project version string for JASwiftBase.
+FOUNDATION_EXPORT const unsigned char JASwiftBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JASwiftBase/PublicHeader.h>
+#import <JASwiftBase/JAAutoFooter.h>
+#import <JASwiftBase/JARefreshHeader.h>
+
